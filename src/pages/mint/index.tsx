505dd6412b209abaa4ff4a1577d1999786da13bb
bpolitiadis <b.politiadis@gmail.com>
2023-01-27 12:25:26 +0200

    added comments

--- src/pages/mint/index.tsx
+++ src/pages/mint/index.tsx
@@ -1,19 +1,24 @@
-import { Heading, Text, Button, MenuItem, FormControl, Select, Image, Container, Spacer, Box } from '@chakra-ui/react'
+import { Heading, Text, Button, Image, Spacer, Box } from '@chakra-ui/react'
 import { Head } from 'components/layout/Head'
 import React, { useEffect, useRef, useState } from 'react'
-import { Configuration, ImagesResponseDataInner, OpenAIApi } from 'openai'
-import { useAccount, useContractWrite, usePrepareContractWrite, useWaitForTransaction } from 'wagmi'
-import { ethers } from 'ethers'
+import { ImagesResponseDataInner, OpenAIApi } from 'openai'
+import { useAccount } from 'wagmi'
 import abi from '../../../constants/abi.json'
 import css from '../../styles/mint.module.css'
 import { Address, readContract } from '@wagmi/core'
-import { useDebounce } from 'usehooks-ts'
 import CreateFighter from 'components/mint/CreateFighter'
 import MintFighter from 'components/mint/MintFighter'
 
+/**
+ * The Mint component is the main page for creating and minting unique fighters on the blockchain.
+ * It uses the `CreateFighter` and `MintFighter` components to handle the creation and minting process, respectively.
+ * It also utilizes the `useAccount` hook from `wagmi` to check the user's wallet connection status.
+ */
 export default function Mint() {
+  // Get the user's address and connection status from the `wagmi` hook
   const { address: user, isConnecting, isDisconnected, isConnected } = useAccount()
 
+  // State to hold the binary image data for the created fighter
   const [imagesBinaryData, setImagesBinaryData] = useState<ImagesResponseDataInner[]>([])
 
   return (
@@ -23,17 +28,21 @@ export default function Mint() {
         <Heading as="h3" textAlign="center" fontFamily="fantasy">
           Mint Page
         </Heading>
+        {/* Only show the creation and minting components if the user is connected to their wallet */}
         {isConnected && (
           <>
             <Box className={css.root}>
               <Box className={css.container}>
+                {/* Pass the `setImagesBinaryData` function as a prop to the `CreateFighter` component */}
                 <CreateFighter setImagesBinaryData={setImagesBinaryData} />
                 <Spacer m="16px" />
+                {/* Pass the `imagesBinaryData` state as a prop to the `MintFighter` component */}
                 <MintFighter imagesBinaryData={imagesBinaryData} />
               </Box>
             </Box>
           </>
         )}
+        {/* Show a message if the user is not connected to their wallet */}
         {!isConnected && (
           <>
             <Text m="32px" fontStyle="oblique" textAlign="center">
