af2e72550c788c63eaf592dc0febc533e377d9f7
bpolitiadis <b.politiadis@gmail.com>
2023-03-02 11:06:27 +0200

    added FighterModal common component

--- src/components/common/FighterModal.tsx
+++ src/components/common/FighterModal.tsx
@@ -1,28 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 import {
+  Badge,
   Box,
-  Container,
-  Flex,
-  Heading,
-  Spacer,
+  Divider,
+  IconButton,
+  Link,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
   Stack,
-  StackDivider,
   Text,
   Image,
-  Divider,
-  Link,
-  Button,
-  ModalOverlay,
-  Modal,
-  ModalContent,
-  ModalCloseButton,
-  ModalHeader,
-  ModalBody,
-  useDisclosure,
-  Badge,
 } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { FighterCardProps, Rarities, RarityColors } from 'components/fight/FighterCard'
+import React, { useState } from 'react'
 
 export const FighterModal = ({
   fighter,
@@ -39,6 +33,12 @@ export const FighterModal = ({
   isOpen: boolean
   onClose: any
 }) => {
+  const [showOwnerInfo, setShowOwnerInfo] = useState(false)
+
+  const toggleOwnerInfo = () => {
+    setShowOwnerInfo(!showOwnerInfo)
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -47,8 +47,8 @@ export const FighterModal = ({
           Fighter #{fighter}
         </ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          <Box>
+        <ModalBody display="flex" flexDirection="column" position="relative">
+          <Box flex="1">
             <Stack>
               <Image
                 border="4px"
@@ -68,20 +68,35 @@ export const FighterModal = ({
               <Divider m="8px" />
             </Stack>
             <Text marginTop="8px">Victories: {fighterStats.victories}</Text>
-            <Divider m="16px" />
-            <Text textAlign="center" justifyContent="center">
-              Owner:{' '}
+            {/* <Divider m="16px" /> */}
+          </Box>
+          <Box
+            bg="white"
+            position="absolute"
+            top="0"
+            right="0"
+            bottom="0"
+            width={showOwnerInfo ? '100%' : '0'}
+            transition="width 0.3s ease-in-out"
+            overflow="hidden">
+            <Box py="2" px="4" textAlign="left">
+              <Text fontWeight="semibold" mb="2">
+                Owner:
+              </Text>
               <Link
-                flexWrap="wrap"
                 fontWeight="light"
                 target="_blank"
                 rel="noopener noreferrer"
-                href={`https://mumbai.polygonscan.com/address/${owner}`}>
-                View on Explorer <ExternalLinkIcon />
+                href={`https://mumbai.polygonscan.com/address/${owner}`}
+                _hover={{ textDecoration: 'underline' }}>
+                {owner}
               </Link>
-            </Text>
+            </Box>
           </Box>
         </ModalBody>
+        {/* <Box py="2" display="flex" justifyContent="flex-end" position="absolute" bottom="0" right="0" width="100%">
+          <IconButton aria-label="Show Owner Info" icon={<ChevronRightIcon />} variant="ghost" size="sm" onClick={toggleOwnerInfo} />
+        </Box> */}
       </ModalContent>
     </Modal>
   )
