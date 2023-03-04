1ee74b0ecf378ea84bd935bffb149d8727098515
bpolitiadis <b.politiadis@gmail.com>
2023-03-04 18:32:46 +0200

    added responsiveness to home page

--- constants/dictionary.ts
+++ constants/dictionary.ts
@@ -24,3 +24,17 @@ export const messages = {
     "You just got beat at your own game, but don't worry, you'll come back even better next time.",
   ],
 }
+
+export const content = {
+  home_title: 'Welcome to Ultimate Fighting Metaverse',
+  home_description1:
+    'Welcome to the world of web3 fighting, where the only limit is your imagination (and the capabilities of OpenAI’s DALL-E, but let’s not get bogged down in technicalities). Are you tired of playing as the same old generic fighters in your favorite fighting games? Well, look no further because we’ve got the solution for you! Our web3 dApp allows you to mint your very own unique fighter with the help of DALL-E’s AI generated images.',
+  home_description2:
+    'Ready to create your own personal powerhouse? Head on over to our mint page and select the external characteristics of your desired fighter. Don’t worry, we’ll take care of the rest. DALL-E will generate four images for you to choose from, and voila! You’ve got yourself a one-of-a-kind NFT fighter with random stats and rarity. So what are you waiting for? Go mint your own champion and show the world who’s boss.',
+  home_description3:
+    'But what’s the point of having your own personal fighter if you can’t show off your skills in the ring? Head over to our fight page and choose one of your minted fighters to join one of our arenas. The more you play, the more your fighter’s stats will increase. So go ahead, put your fighter to the test and dominate the competition.',
+  home_description4:
+    'But let’s not forget about the technologies that make all of this possible. We’re talking about OpenAI’s DALL-E, Ethereum Solidity, Nextjs, Web3, and the Metaverse. With these cutting-edge technologies, we’re able to bring you a truly unique and immersive web3 fighting experience. So thank you, technology, for making our wildest dreams a reality.',
+  home_description5:
+    'In conclusion, our web3 dApp fighting game is the perfect blend of AI-generated uniqueness and competitive gameplay. So go mint your own fighter, put them to the test, and show the world who’s boss. With the help of OpenAI, Ethereum, and the Metaverse, the possibilities are endless. See you in the ring!',
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import styles from 'styles/Home.module.css'
-import { Flex, Box, Image, Heading, Text, Link, Center, Container } from '@chakra-ui/react'
+import { Flex, Box, Image, Heading, Text, Link, Center, Container, Stack } from '@chakra-ui/react'
 import { Head } from 'components/layout/Head'
 import { SITE_DESCRIPTION, SITE_NAME } from 'utils/config'
+import { content } from '../../constants/dictionary'
 
 export default function Home() {
   const images = [
@@ -15,51 +16,43 @@ export default function Home() {
     <>
       <Box px={6} py={8}>
         <Flex justifyContent="center" flexDirection="column" alignItems="center">
-          <Box alignItems="center" px={6} py={8} justifyContent="center">
-            <Heading as="h1" size="xl" textAlign="center" justifyContent="center" mx="auto">
-              Welcome to Ultimate Fighting Metaverse
+          <Box alignItems="center" px={6} py={8} justifyContent="center" textAlign="center">
+            <Heading as="h1" size={['lg', 'xl']} textAlign="center" justifyContent="center" mx="auto">
+              {content.home_title}
             </Heading>
-            <Text textAlign="center" my={4}>
-              Welcome to the world of web3 fighting, where the only limit is your imagination (and the capabilities of OpenAI&apos;s DALL-E, but
-              let&apos;s not get bogged down in technicalities). Are you tired of playing as the same old generic fighters in your favorite fighting
-              games? Well, look no further because we&apos;ve got the solution for you! Our web3 dApp allows you to mint your very own unique fighter
-              with the help of DALL-E&apos;s AI generated images.
+            <Text textAlign="center" my={4} fontSize={['mg', 'lg']}>
+              {content.home_description1}
             </Text>
 
             <Box alignItems="center" px={6} py={8} justifyContent="center">
-              <Image src={images[0]} alt="Mint your fighter" width="100%" textAlign="center" margin="0 auto" />
+              <Image src={images[0]} alt="fighter-collage-1" maxWidth={['100%', '80%', '60%']} margin="0 auto" />
             </Box>
 
-            <Text textAlign="center" my={4}>
-              Ready to create your own personal powerhouse? Head on over to our mint page and select the external characteristics of your desired
-              fighter. Don&apos;t worry, we&apos;ll take care of the rest. DALL-E will generate four images for you to choose from, and voila!
-              You&apos;ve got yourself a one-of-a-kind NFT fighter with random stats and rarity. So what are you waiting for? Go mint your own
-              champion and show the world who&apos;s boss.
+            <Text textAlign="center" my={4} fontSize={['md', 'lg']}>
+              {content.home_description2}
             </Text>
 
             <Box alignItems="center" px={6} py={8} justifyContent="center">
-              <Image src={images[1]} alt="Mint your fighter" width="100%" textAlign="center" margin="0 auto" />
+              <Image src={images[1]} alt="arena-collage-1" maxWidth={['100%', '80%', '60%']} margin="0 auto" />
             </Box>
 
-            <Text textAlign="center" my={4}>
-              But what&apos;s the point of having your own personal fighter if you can&apos;t show off your skills in the ring? Head over to our fight
-              page and choose one of your minted fighters to join one of our arenas. The more you play, the more your fighter&apos;s stats will
-              increase. So go ahead, put your fighter to the test and dominate the competition.
+            <Text textAlign="center" my={4} fontSize={['md', 'lg']}>
+              {content.home_description3}
             </Text>
-            <Flex alignItems="center" px={6} py={8} justifyContent="space-between" flexDir="row">
-              <Image src={images[2]} alt="Dall-E" width="40%" textAlign="center" />
-              <Image src={images[3]} alt="Dall-E" width="50%" textAlign="center" />
+
+            <Flex alignItems="center" px={6} py={8} justifyContent="space-around" flexDir={['column', 'row']}>
+              <Image src={images[2]} alt="openai-logo" maxWidth={['100%', '40%']} textAlign="center" margin="0 auto" />
+              <Image src={images[3]} alt="dalle-logo" maxWidth={['100%', '50%']} textAlign="center" margin="0 auto" />
             </Flex>
-            <Text textAlign="center" my={4}>
-              But let&apos;s not forget about the technologies that make all of this possible. We&apos;re talking about OpenAI&apos;s DALL-E, Ethereum
-              Solidity, Nextjs, Web3, and the Metaverse. With these cutting-edge technologies, we&apos;re able to bring you a truly unique and
-              immersive web3 fighting experience. So thank you, technology, for making our wildest dreams a reality.
-            </Text>
-            <Text textAlign="center" my={4}>
-              In conclusion, our web3 dApp fighting game is the perfect blend of AI-generated uniqueness and competitive gameplay. So go mint your own
-              fighter, put them to the test, and show the world who&apos;s boss. With the help of OpenAI, Ethereum, and the Metaverse, the
-              possibilities are endless. See you in the ring!
-            </Text>
+
+            <Stack spacing={4} px={6} py={8} maxWidth="800px" textAlign="center">
+              <Text my={4} fontSize={['md', 'lg']}>
+                {content.home_description4}
+              </Text>
+              <Text my={4} fontSize={['md', 'lg']}>
+                {content.home_description5}
+              </Text>
+            </Stack>
           </Box>
         </Flex>
       </Box>
